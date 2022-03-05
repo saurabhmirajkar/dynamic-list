8c07bbd89e666dd039647185ba6bfa1878412de6
Saurabh Mirajkar <mirajkar46@gmail.com>
2022-03-05 22:48:10 +0530

    Initial Commit

---
+++ DynamicList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicList
+//
+//  Created by Saurabh Mirajkar on 05/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
